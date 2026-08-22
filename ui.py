0a98e90dd5e4f010b43b0243f9792dee97d4923e
asyncing <swastikban06@gmail.com>
2026-08-22 23:29:15 +0530

    Streamlit tests completed

--- ui.py
+++ ui.py
@@ -1,1 +1,12 @@
-import streamlit
+import streamlit as st
+st.set_page_config(page_title="DocScannerLLM", page_icon="✡")
+st.title("Test Code")
+st.write("Checking Browser")
+st.divider()
+st.header("Interactive")
+user=st.text_input("Enter gibberish")
+slider_val=st.slider("Check Slider", 1, 1000, 69)
+st.header("Buttons")
+if st.button("Click here"):
+    st.snow()
+    st.success("Test Complete")
